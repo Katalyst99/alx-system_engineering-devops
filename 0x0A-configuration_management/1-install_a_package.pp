47362e451d541da667a521133e7dcd40e9c533cf
Katalyst99 <katleho333@gmail.com>
2024-06-28 05:36:50 -0700

    Corrected code to meet requirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-#install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#install flask from pip3
+package { 'python3-pip':
+  ensure  => 'installed',
+  require => Exec['apt-get install']
+}
+
+exec { 'apt-get install':
+  command  => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.1.1'
 }
