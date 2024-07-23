f4adc3308cf013582f9115d3d1dde705ec780e35
Katalyst99 <katleho333@gmail.com>
2024-07-23 04:19:19 -0700

    Setup of mysql 3

--- 0x14-mysql/mysql_setup.sql
+++ 0x14-mysql/mysql_setup.sql
@@ -1,21 +1,20 @@
---mysql setup for web-01 and web-02 servers--
 CREATE USER holberton_user@localhost
 IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION CLIENT ON *.*
 TO 'holberton_user'@'localhost';
 CREATE DATABASE tyrell_corp;
 USE tyrell_corp;
-CREATE TABLE nexus6(id INT, name VARCHAR(256));
-INSERT INTO nexus6 (name) VALUES ('Leon');
+CREATE TABLE nexus6(id INT, name TEXT);
+INSERT INTO nexus6 VALUES (0, 'Leon');
 GRANT SELECT ON tyrell_corp.nexus6
 TO holberton_user@localhost;
-CREATE USER 'replica_user'@'%'
-IDENTIFIED BY 'replica;'
+CREATE USER replica_user@'%'
+IDENTIFIED BY 'replica';
 GRANT REPLICATION SLAVE ON *.*
 TO 'replica_user'@'%';
 GRANT SELECT ON mysql.user
 TO holberton_user@localhost;
-CREATE USER web-02@34.224.2.3
-IDENTIFIED BY 'web-02';
+CREATE USER web02@34.224.2.3
+IDENTIFIED BY 'web02';
 GRANT REPLICATION SLAVE ON *.*
-TO web-02@34.224.2.3;
+TO web02@34.224.2.3;
