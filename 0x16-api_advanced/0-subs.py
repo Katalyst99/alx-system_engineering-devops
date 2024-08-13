843250425db52d75426e398c9a1504fba294f94c
Katalyst99 <katleho333@gmail.com>
2024-08-13 04:45:01 -0700

    Fixing requirement errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Katalyst'}
     resp = requests.get(url, headers=headers, allow_redirects=False)
     if resp.status_code == 200:
